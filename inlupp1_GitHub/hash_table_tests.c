ad5dd910e774cd2297ef271a0d1e26128b47c823
OliverHansson <90697655+OliverHansson@users.noreply.github.com>
2022-09-15 20:28:10 +0200

    test

--- inlupp1_GitHub/hash_table_tests.c
+++ inlupp1_GitHub/hash_table_tests.c
@@ -14,16 +14,14 @@ void test_create_destroy()
 }
 
 void test_insert_once() {
-   ioopm_hash_table_t *ht = ioopm_hash_table_create();
-   CU_ASSERT_PTR_NULL(ioopm_hash_table_lookup(ht, 1));
-   ioopm_hash_table_insert(ht,1, "hej");
-   
-   CU_ASSERT_PTR_NOT_NULL(ioopm_hash_table_lookup(ht, 1));
-
-   ioopm_hash_table_destroy(ht);
+   ioopm_hash_table_t *ht = ioopm_hash_table_create(); //Skapa ht
+   CU_ASSERT_PTR_NULL(ioopm_hash_table_lookup(ht, 1)); //Kolla om key 1 är NULL
+   ioopm_hash_table_insert(ht,1, "hej"); //Lägg till value "hej" i key 1
+   CU_ASSERT_PTR_NOT_NULL(ioopm_hash_table_lookup(ht, 1)); //TODO vad key 1 mappar till
+   ioopm_hash_table_destroy(ht); //destroy ht
 }
 
 
 int main () {
-   
+
 }

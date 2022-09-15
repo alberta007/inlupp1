0d8e2b8f21f6aba2332dfcc4443bf8a1c7918b31
alberta007 <63005220+alberta007@users.noreply.github.com>
2022-09-15 16:05:19 +0200

    Update hash_table_tests.c

--- inlupp1_GitHub/hash_table_tests.c
+++ inlupp1_GitHub/hash_table_tests.c
@@ -13,7 +13,17 @@ void test_create_destroy()
    ioopm_hash_table_destroy(ht);
 }
 
+void test_insert_once() {
+   ioopm_hash_table_t *ht = ioopm_hash_table_create();
+   CU_ASSERT_PTR_NULL(ioopm_hash_table_lookup(ht, 1));
+   ioopm_hash_table_insert(ht,1, "hej");
+   
+   CU_ASSERT_PTR_NOT_NULL(ioopm_hash_table_lookup(ht, 1));
+
+   ioopm_hash_table_destroy(ht);
+}
+
 
 int main () {
-
+   
 }
